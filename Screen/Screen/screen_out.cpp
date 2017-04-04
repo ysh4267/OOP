d5be79afe25feb491b038fdf2bd09f1abd3a1ba2
beomjoo90 <beomjoo90@gmail.com>
2017-04-04 17:52:57 +0900

    플레이어 drawing 추상화
    strncpy, strcpy, strlen 사용 예제

--- Screen/Screen/screen_out.cpp
+++ Screen/Screen/screen_out.cpp
@@ -1,42 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
 #include <Windows.h>
 
-#define SCREEN_SIZE 79
+#define SCREEN_SIZE	79
+
+void draw(char *screen, int bullet_pos, int direction)
+{
+	screen[bullet_pos] = (direction == 0) ? '>' : '<';
+}
+
+void update(int* bullet_pos, int* direction)
+{
+	if (*direction == 0)
+	{
+		(*bullet_pos)++;
+		if (*bullet_pos >= SCREEN_SIZE) {
+			*direction = 1;
+			*bullet_pos = SCREEN_SIZE - 1;
+		}
+	}
+	else {
+		(*bullet_pos)--;
+		if (*bullet_pos < 0) {
+			*direction = 0;
+			*bullet_pos = 0;
+		}
+	}
+}
 
 int main()
 {
+	int bullet_pos = 0;
 	char screen[SCREEN_SIZE + 1];
-	int bullet_pos = 30;
-	char bullet_shape = '>';
-	char player_face[4] = "^_^";
-	char enemy_face[] = "*_____*";
-	int enemy_pos = 50;
-	int player_pos = 20;
+	int direction = 0;
+	char face[] = "^___________^";
 
-	
-	while (true) {
+	while (1)
+	{
 		memset(screen, ' ', SCREEN_SIZE);
 
-		int ch = _getch();
-		enemy_pos = enemy_pos + (rand() % 3 - 1);
-		//enemy_pos %= SCREEN_SIZE;
-		
-		screen[bullet_pos] = bullet_shape;
-		strncpy(screen + player_pos, player_face, strlen(player_face));
-		strncpy(screen + enemy_pos, enemy_face, strlen(enemy_face));
+		draw(screen, bullet_pos, direction);	
+		strncpy(screen + 10, face, strlen(face));
 
+		update(&bullet_pos, &direction);
 
 		screen[SCREEN_SIZE] = '\0';
 		printf("%s\r", screen);
-
-		Sleep(33);
-
+		Sleep(33);		
 	}
 	return 0;
-
 
 }
