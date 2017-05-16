f106339d69f70a436d31feb1b065a90ab9ab2cad
beomjoo90 <beomjoo90@gmail.com>
2017-05-16 10:13:18 +0900

    Player, Enemey, Bullet 클래스화
    Bullet내 GameObject가 있음을 유의

--- Screen/Screen/screen_out.cpp
+++ Screen/Screen/screen_out.cpp
@@ -5,140 +5,188 @@
 #include <Windows.h>
 #include <conio.h>
 
+#define SCREEN_SIZE		79
+
 using namespace std;
 
-#define SCREEN_SIZE	79
-
-struct GameObject {
+class GameObject {
+	char 	shape[16];
 	int		pos;
-	char	shape[20];
 
-	void	draw(char *screen) const
+public:
+	GameObject(const char *shape, int pos) : pos(pos) { strcpy(this->shape, shape); }
+	
+	void SetShape(const char *shape) { strcpy(this->shape, shape); }
+
+	void SetPos(int pos) { this->pos = pos; }
+	int  GetPos() const { return this->pos; }
+
+	void IncreasePos() { this->pos++; }
+	void DecreasePos() { this->pos--; }	
+
+	void Draw(char *canvas) const
 	{
-		strncpy(screen + pos, shape, strlen(shape));
+		if (pos < 0 || pos >= SCREEN_SIZE) return;
+
+		strncpy(canvas + pos, shape, strlen(shape) );
 	}
 };
 
-struct Bullet {
-	struct GameObject	myData;
+class Bullet {
+	GameObject  myData;
+	bool 		isFired;
+	GameObject* target;
 
-	void	draw(char *screen, const struct GameObject *player, const struct GameObject *enemy) const
-	{
-		if (myData.pos == -1) return;
+	void reset() { isFired = false; }
 
-		if (screen == NULL || player == NULL || enemy == NULL) return;
+public:
+	Bullet() : isFired(false), myData( ">", 0), target(nullptr) {}
+
+	void Fire(const GameObject& player, GameObject& enemy)
+	{ 
+		int player_pos = player.GetPos();
+		int target_pos = enemy.GetPos();
 		
-		if (player->pos < enemy->pos) {
-			strncpy(screen + myData.pos, ">", strlen(">"));
+		if (player_pos == target_pos) return;
+
+		if (player_pos < target_pos) {
+			myData.SetShape(">");
+		} else {			
+			myData.SetShape("<");
 		}
-		else if (player->pos > enemy->pos) {
-			strncpy(screen + myData.pos, "<", strlen("<"));
+
+		SetPos( player_pos );
+		target = &enemy;
+		isFired = true;
+	}
+
+	void SetPos(int pos) { myData.SetPos(pos); }
+	int GetPos() const { return myData.GetPos(); }
+
+	void IncreasePos() { myData.IncreasePos(); }
+	void DecreasePos() { myData.DecreasePos(); }
+
+	bool IsFired() { return isFired; }
+
+	void Update()
+	{
+		if (isFired == false || !target) return;
+
+		int pos = myData.GetPos();
+		int target_pos = target->GetPos();
+
+		if (pos < 0 || pos >= SCREEN_SIZE)
+		{
+			reset();
+			return;
 		}
+
+		if (pos == target_pos) 
+		{
+			reset();
+			return;
+		}
+
+		if (pos < target_pos) {
+			IncreasePos();
+		} else {
+			DecreasePos();
+		}
+	}	
+
+	void Draw(char *canvas) const
+	{
+		if (isFired == false || target == nullptr) return;
+
+		myData.Draw(canvas);
 	}
 };
 
-
-void clear_screen(char *screen)
+void clear_screen(char *canvas)
 {
-	memset(screen, ' ', SCREEN_SIZE);
+	memset(canvas, ' ', SCREEN_SIZE);
+	canvas[SCREEN_SIZE] = '\0';
 }
 
-void draw(char *screen, const struct GameObject* player, const struct GameObject* enemy, const struct Bullet bullets[], int bullets_len)
-{
-	if (screen == NULL) return;
-
-	player->draw(screen);
-	enemy->draw(screen);
-
-	for (int i = 0; i < bullets_len; i++)
-	{
-		bullets[i].draw(screen, player, enemy);
-	}
-}
-
-void update(const struct GameObject* player, const struct GameObject* enemy, struct Bullet bullets[], int bullets_len)
-{
-	for (int i = 0; i < bullets_len; i++)
-	{
-		if (bullets[i].myData.pos == -1) continue;
-
-		if (bullets[i].myData.pos == enemy->pos)
-		{
-			bullets[i].myData.pos = -1;
-			continue;
-		}
-
-		if (bullets[i].myData.pos < 0 || bullets[i].myData.pos >= SCREEN_SIZE)
-		{
-			bullets[i].myData.pos = -1;
-			continue;
-		}
-
-		if (player->pos < enemy->pos)		bullets[i].myData.pos++;
-		else if (player->pos > enemy->pos)	bullets[i].myData.pos--;
-	}
-}
-
-void render(char *screen)
-{
-	screen[SCREEN_SIZE] = '\0';
-	cout << screen << '\r';
-	Sleep(33);
-}
-
-void process_input(struct GameObject* player, struct GameObject* enemy, struct Bullet bullets[], int bullets_len)
+void process_input(GameObject& player, GameObject& enemy, Bullet bullets[], int bullets_len)
 {
 	if (_kbhit() == 0) return;
 
 	int ch = _getch();
-
 	if (ch == 224)
 	{
 		ch = _getch();
-		if (ch == 75) player->pos--;
-		if (ch == 77) player->pos++;
-		if (ch == 80) enemy->pos++;
-		if (ch == 72) enemy->pos--;
+		switch (ch)
+		{
+		case 75:
+			player.IncreasePos();
+			break;
+		case 77:
+			player.DecreasePos();
+			break;
+		case 72:
+			enemy.IncreasePos();
+			break;
+		case 80:
+			enemy.DecreasePos();
+			break;
+		}
 	}
 	else if (ch == ' ')
 	{
 		for (int i = 0; i < bullets_len; i++)
 		{
-			if (bullets[i].myData.pos != -1) continue;
-
-			// found a bullet ready to shoot
-			bullets[i].myData.pos = player->pos;
-			break; // exit from the loop
+			if (bullets[i].IsFired() == false)
+			{
+				bullets[i].Fire(player, enemy);
+				break; // exit from for statement
+			}
 		}
 	}
 }
 
-int main()
-{	
-	char screen[SCREEN_SIZE + 1];
-	const int bullets_len = 30;
-	struct GameObject player;
-	struct GameObject enemy;
-	struct Bullet bullets[bullets_len];
+void draw(char *canvas, const GameObject& player, const GameObject& enemy, const Bullet bullets[], int bullets_len)
+{
+	if (canvas == NULL) return;
 
-	strcpy(player.shape, "^_^");
-	player.pos = rand() % SCREEN_SIZE;
-	strcpy(enemy.shape, "*__*");
-	enemy.pos = rand() % SCREEN_SIZE;
+	player.Draw(canvas);
+	enemy.Draw(canvas);
+	for (int i= 0; i < bullets_len; i++)
+	{
+		bullets[i].Draw(canvas);
+	}
+}
+
+void update(const GameObject& player, const GameObject& enemy, Bullet bullets[], int bullets_len)
+{
 	for (int i = 0; i < bullets_len; i++)
 	{
-		strcpy(bullets[i].myData.shape, ">");
-		bullets[i].myData.pos = -1;
+		bullets[i].Update();
 	}
-	
+}
+
+void render(char *canvas)
+{
+	canvas[SCREEN_SIZE] = '\0';
+	cout << canvas << '\r';
+	Sleep(33);
+}
+
+int main()
+{
+	char	canvas[SCREEN_SIZE + 1];
+	GameObject player("^_^", rand() % SCREEN_SIZE);	
+	GameObject enemy("*_*", rand() % SCREEN_SIZE);	
+	const int bullets_len = 30;
+	Bullet bullets[bullets_len];
+
 	while (1)
 	{
-		clear_screen(screen);
-		process_input(&player, &enemy, bullets, bullets_len);
-		draw(screen, &player, &enemy, bullets, bullets_len);
-		update( &player, &enemy, bullets, bullets_len);
-		render(screen);			
+		clear_screen(canvas);
+		process_input(player, enemy, bullets, bullets_len);
+		draw(canvas, player, enemy, bullets, bullets_len);
+		update(player, enemy, bullets, bullets_len);
+		render(canvas);
 	}
 	return 0;
-
 }
